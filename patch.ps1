7cd81e6a610db875425b2cbfc0f175c0ba4d364c
ActiveIce <oyab1053544791@gmail.com>
2022-03-31 16:44:55 +0800

    Add files via upload

--- patch.ps1
+++ patch.ps1
@@ -8,7 +8,7 @@ function Test-Administrator
     }
 }
 
-if(-not (Test-Administrator))
+if (-not (Test-Administrator))
 {
     # TODO: define proper exit codes for the given errors 
     Write-Error "This script must be executed as Administrator.";
@@ -16,21 +16,79 @@ if(-not (Test-Administrator))
 }
 
 $Path_Acrobat = "C:\Program Files\Adobe\Acrobat DC\Acrobat\"
-$bOriginal_Acrobat = [System.Byte[]](0xD8, 0xE8, 0x2A, 0x04, 0x00, 0x00, 0x66, 0x85, 0xC0, 0x74, 0x1A, 0x66, 0x85, 0xDB, 0x0F, 0x85, 0xAD, 0x02, 0x00, 0x00, 0x8B, 0xD6, 0xB9, 0x2C)
-$bSubstitute_Acrobat = [System.Byte[]](0xD8, 0xE8, 0x2A, 0x04, 0x00, 0x00, 0x66, 0x85, 0xC0, 0x74, 0x00, 0x66, 0x85, 0xDB, 0x0F, 0x85, 0xAD, 0x02, 0x00, 0x00, 0x8B, 0xD6, 0xB9, 0x2C)
-$bOriginal_acrodistdll = [System.Byte[]](0x3E, 0x00, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0x48, 0x89, 0x5C, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24, 0x18, 0x48, 0x89, 0x7C, 0x24, 0x20, 0x55)
-$bSubstitute_acrodistdll = [System.Byte[]](0x3E, 0x00, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0x33, 0xC0, 0xC3, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24, 0x18, 0x48, 0x89, 0x7C, 0x24, 0x20, 0x55)
+$bOriginal_Acrobat = [System.Byte[]](0x00, 0x00, 0x66, 0x85, 0xC0, 0x74, 0x1A, 0x66, 0x85, 0xDB, 0x0F, 0x85)
+$bSubstitute_Acrobat = [System.Byte[]](0x00, 0x00, 0x66, 0x85, 0xC0, 0x74, 0x00, 0x66, 0x85, 0xDB, 0x0F, 0x85)
+$bOriginal_acrodistdll = [System.Byte[]](0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0x48, 0x89, 0x5C, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24, 0x18, 0x48, 0x89, 0x7C, 0x24, 0x20, 0x55, 0x41, 0x54, 0x41, 0x55, 0x41, 0x56, 0x41, 0x57, 0x48, 0x8D, 0xAC, 0x24, 0x00)
+$bSubstitute_acrodistdll = [System.Byte[]](0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0x33, 0xC0, 0xC3, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24, 0x18, 0x48, 0x89, 0x7C, 0x24, 0x20, 0x55, 0x41, 0x54, 0x41, 0x55, 0x41, 0x56, 0x41, 0x57, 0x48, 0x8D, 0xAC, 0x24, 0x00)
 
-cd $Path_Acrobat
+
+#cd $Path_Acrobat
 echo "Patching Acrobat.dll"
 Copy-Item -Path Acrobat.dll -Destination Acrobat.dll.bak
 $bInput_Acrobat = Get-Content -Path Acrobat.dll -Encoding Byte -Raw
-$bOutput_Acrobat = "$bInput_Acrobat" -Replace "\b$bOriginal_Acrobat\b", "$bSubstitute_Acrobat" -Split '\s+' -as [System.Byte[]]
-Set-Content -Path Acrobat.dll -Encoding Byte -Value $bOutput_Acrobat
+$Offset_Acrobat = $bInput_Acrobat.Length-$bOriginal_Acrobat.Length
+$i = 0
+for(; $i -le $Offset_Acrobat; $i++)
+{
+    $j = 0
+    for (; $j -lt $bOriginal_Acrobat.Length; $j++)
+    {
+        if ($bInput_Acrobat[$i+$j] -ne $bOriginal_Acrobat[$j])
+        {
+            break
+        }
+    }
+    if ($j -eq $bOriginal_Acrobat.Length)
+    {
+        break
+    }
+}
+if ($i -le $Offset_Acrobat)
+{
+    for ($j = 0; $j -lt $bOriginal_Acrobat.Length; $j++)
+    {
+        $bInput_Acrobat[$i+$j] = $bSubstitute_Acrobat[$j]
+    }
+    Set-Content -Path Acrobat.dll -Encoding Byte -Value $bInput_Acrobat
+    echo "Done"
+}
+else
+{
+    echo "Fail"
+}
+
 echo "Patching acrodistdll.dll"
 Copy-Item -Path acrodistdll.dll -Destination acrodistdll.dll.bak
 $bInput_acrodistdll = Get-Content -Path acrodistdll.dll -Encoding Byte -Raw
-$bOutput_acrodistdll = "$bInput_acrodistdll" -Replace "\b$bOriginal_acrodistdll\b", "$bSubstitute_acrodistdll" -Split '\s+' -as [System.Byte[]]
-Set-Content -Path acrodistdll.dll -Encoding Byte -Value $bOutput_acrodistdll
-echo "Done"
+$Offset_acrodistdll = $bInput_acrodistdll.Length-$bOriginal_acrodistdll.Length
+$i = 0
+for(; $i -le $Offset_acrodistdll; $i++)
+{
+    $j = 0
+    for (; $j -lt $bOriginal_acrodistdll.Length; $j++)
+    {
+        if ($bInput_acrodistdll[$i+$j] -ne $bOriginal_acrodistdll[$j])
+        {
+            break
+        }
+    }
+    if ($j -eq $bOriginal_acrodistdll.Length)
+    {
+        break
+    }
+}
+if ($i -le $Offset_acrodistdll)
+{
+    for ($j = 0; $j -lt $bOriginal_acrodistdll.Length; $j++)
+    {
+        $bInput_acrodistdll[$i+$j] = $bSubstitute_acrodistdll[$j]
+    }
+    Set-Content -Path acrodistdll.dll -Encoding Byte -Value $bInput_acrodistdll
+    echo "Done"
+}
+else
+{
+    echo "Fail"
+}
+
 pause
